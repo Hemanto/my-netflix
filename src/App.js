106ed79ef3c1edb71a9929f502b1b678d38b6f72
Hemanto Das <mehemanto@gmail.com>
2023-05-20 12:34:01 +0000

    hero content section

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import HeroContent from "./Components/HeroContent";
 import React from "react";
 import Header from "./Components/Header";
 
+import "./App.css";
+
+import HeroContent from "./Components/HeroContent";
+
 function App() {
   return (
     <div>
-      <HeroContent />
-
       <Header />
+
+      <HeroContent />
     </div>
   );
 }
