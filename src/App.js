9ab723e0e44953aff83a51208dc3f808dee5b51b
SiddiquiSaud <siddiquisaud1999@gmail.com>
2023-05-20 23:56:15 +0530

    Added Header in Netflix clone

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import { Provider } from "react-redux";
-import "./App.css";
-import Head from "./Components/Head";
-import MainContainer from "./Layouts/MainContainer";
-//import Todo from "./Layouts/Todo";
-import store from "./Utility/store";
+import React from "react";
+import Header from "./Components/Header";
 
 function App() {
   return (
-    <Provider store={store}>
       <div>
-        <Head />
-        <MainContainer />
+        <Header />
       </div>
-    </Provider>
   );
 }
 
